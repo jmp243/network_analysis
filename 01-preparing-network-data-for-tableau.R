7c64ddaf8a64b60f7156187d403b8c6222fcac58
Jung Mee Park <jmpark@arizona.edu>
2024-04-04 21:58:22 -0700

    reversed direction for base 2 connections

--- 01-preparing-network-data-for-tableau.R
+++ 01-preparing-network-data-for-tableau.R
@@ -10,6 +10,7 @@ library(readr)
 library(knitr)
 library(stringr)
 library(lubridate)
+# library(rgexf)
 
 # read in data
 initial_report_SF <- read_csv("~/Documents/Trellis/Network_Analysis_Tableau/data/initial-report-SF2.csv")
@@ -67,11 +68,22 @@ write.csv(d3,"~/Documents/Trellis/Network_Analysis_Tableau/data/output-report-SF
 
 # more than one Edge
 # create an edges csv
-edges <- d3 %>% 
-  select(`Advisor Name`, new_id) %>% 
-  rename(Source = "Advisor Name") %>% 
-  rename(Target = new_id)
+# adding the new ID for source/ advisor name
 
+Source_col <- d3 %>% 
+  select(`Advisor Name`) %>% 
+  distinct() %>% 
+  mutate(Source = rownames(.))
+
+d3 <- d3 %>% 
+  left_join(Source_col)
+
+edges <- d3 %>% 
+  select(Source, new_id) %>% 
+  # mutate(Source = rownames("Advisor Name")) %>% 
+  rename(Target = new_id) %>% 
+  distinct()
+ 
 edges$Weight <- "1"
 edges$Type <- "Directed" 
 
@@ -80,7 +92,71 @@ write.csv(edges,"~/Documents/Trellis/Network_Analysis_Tableau/data/edges.csv")
 
 # create nodes sheet
 nodes <- d3 %>% 
-  select(owner_id, `Advisor Name`) %>% 
+  select(`Advisor Name`, owner_id, Source) %>% 
   unique()
+
+nodes_gephi <- d3 %>% 
+  select(owner_id, Source) %>% 
+  rename(Id = Source) %>% 
+  unique()
+
 # write nodes csv
 write.csv(nodes,"~/Documents/Trellis/Network_Analysis_Tableau/data/nodes.csv")
+# write.csv(nodes2,"~/Documents/Trellis/Network_Analysis_Tableau/data/nodes2.csv")
+write.csv(nodes_gephi,"~/Documents/Trellis/Network_Analysis_Tableau/data/nodes_gephi.csv")
+
+
+
+# # reading in a GEPHI file
+# # Accessing the path of the file
+# fn    <- system.file(
+#   "../gephi2.gexf", package = "rgexf"
+# )
+# gephi <- read.gexf(fn)
+
+# bringing in the X Target and Y Target csv
+tableau_xy <- read_csv("~/Documents/Trellis/Network_Analysis_Tableau/data/gephi_output_from_xml.csv")
+tableau_xy$Source <- as.character(tableau_xy$Source)
+
+tableau_xy <- tableau_xy %>% 
+  left_join(nodes) %>% 
+  # select(-`Attribute:for`) %>% 
+  mutate(base = 1)
+
+tableau_xy2 <- read_csv("~/Documents/Trellis/Network_Analysis_Tableau/data/gephi_output_from_xml.csv")
+tableau_xy2$Source <- as.character(tableau_xy2$Source)
+
+tableau_xy2 <- tableau_xy2 %>% 
+  left_join(nodes) %>% 
+  # select(-`Attribute:for`) %>% 
+  mutate(base = 2)
+
+tableau_table <- rbind(tableau_xy, tableau_xy2)
+
+# bringing in edges from Gephi/xml/excel
+gephi_edges <- read_csv("~/Documents/Trellis/Network_Analysis_Tableau/data/edges_from_xml.csv")
+gephi_edges <- gephi_edges %>% 
+  rename(Source = `Attribute:source`) %>% 
+  rename(Index = `Attribute:id`) %>% 
+  rename(Target = `Attribute:target`)
+gephi_edges$Source <- as.character(gephi_edges$Source)
+  
+# merge tableau table to edges
+network_tableau <- tableau_table %>% 
+  left_join(gephi_edges)
+
+# reduce the data size
+network_tableau2 <- network_tableau %>% 
+  filter(!is.na(Target) | Target != "")
+
+# create a column named direction
+network_tableau2$direction <- paste(network_tableau2$Source, "->", network_tableau2$Target)
+network_tableau2 <- network_tableau2 %>% 
+  select(-`Attribute:value.1`) %>% 
+  distinct()
+
+# write name csv for the tableau table
+write.csv(network_tableau,"~/Documents/Trellis/Network_Analysis_Tableau/data/network_tableau.csv")
+
+write.csv(network_tableau2,"~/Documents/Trellis/Network_Analysis_Tableau/data/network_tableau2.csv")
+# write.csv(nodes2,"~/Documents/Trellis/Network_Analysis_Tableau/data/nodes2.csv")
